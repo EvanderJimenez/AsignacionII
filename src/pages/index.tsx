9fa43e3f9859a1df3cae9a069cd14991666f8f2b
MoraPrince <moraj5391@gmail.com>
2023-04-02 18:34:50 -0600

    utilizacion de useLayoutEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Pokemon from '@/components/Pokemon'
-import Pokeball from '@/components/pokeball/Pokeball'
+import { Inter } from "next/font/google";
+import GifList from "@/components/ListGifs";
+import Pokemon from "@/components/Pokemon";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <Pokemon/>
-  )
+  return <GifList />;
 }
