3dd6aaeebc3075aff99ada5e128660fb9c74d44a
DARIOVRUNA <ramon.vargas.rojas@est.una.ac.cr>
2023-04-15 20:57:11 -1100

    placeholder pokemon

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -121,7 +121,7 @@ const Pokemon = (): JSX.Element => {
                   placeholder=" "
                 />
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-5 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-brown-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-brown-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-brown-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                  Pokémon not found: Re-enter name
+                  Pokemon Name
                 </label>
                 <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
               </div>
@@ -137,7 +137,7 @@ const Pokemon = (): JSX.Element => {
         </form>
         {error && (
           <div className="flex justify-center items-center text-red-500 text-[20px] font-bold">
-            Pokemon not found
+            Pokémon not found: Re-enter name
           </div>
         )}
       </div>
